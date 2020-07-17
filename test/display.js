2907381bb5be2f258f36abbc0978c04e8386da1c
Ryan Berdel <ryan@berdel.rocks>
2020-07-17 05:22:58 -0400

    Added test for two letter words

--- test/display.js
+++ test/display.js
@@ -7,20 +7,23 @@ describe('display', function () {
 
     it('first letter', function () {
       filter = new Filter({ firstLetter: true });
-      filter.addWords('nerd');
+      filter.addWords('nerd', 'dog', 'go');
       assert(filter.clean('damn nerd') == 'd*** n***');
+      assert(filter.clean("go dog go") == "g* d** g*");
     });
 
     it('first + last letter', function () {
       filter = new Filter({ firstLetter: true, lastLetter: true });
-      filter.addWords('nerd');
+      filter.addWords('nerd', 'dog', 'go');
       assert(filter.clean('damn nerd') == 'd**n n**d');
+      assert(filter.clean("go dog go") == "go d*g go");
     });
 
     it('last letter', function () {
       filter = new Filter({ lastLetter: true });
-      filter.addWords('nerd');
+      filter.addWords('nerd', 'dog', 'go');
       assert(filter.clean('damn nerd') == '***n ***d');
+      assert(filter.clean("go dog go") == "*o **g *o");
     });
 
   });
